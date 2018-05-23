9da1dcf6eea367634c019bdcac9db581c0077f51
Carol Chung <cch5ng@gmail.com>
2018-05-22 19:55:36 -0700

    algo solutions 052218

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,77 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// instructor suggested using array.sort()
+// runtime would be n log n (due to sort method)
+function anagrams(str1, str2) {
+  let clean1 = str1.replace(/[^\w]/g).toLowerCase()
+  let clean2 = str2.replace(/[^\w]/g).toLowerCase()
+
+  let ar1 = clean1.split('');
+  ar1.sort();
+  let ar2 = clean2.split('');
+  ar2.sort();
+
+  return ar1.join('') === ar2.join('');
+}
+
+
+
+
+// runtime would be about len str1 + len str2
+// function getCharCount(str) {
+//   let charCount = {};
+
+//   for (let ch of str) {
+
+//     // instructor suggested
+//     charCount[ch] = charCount[ch] + 1 || 1;
+
+//     // if (charCount[ch]) {
+//     //   charCount[ch] += 1;
+//     // } else {
+//     //   charCount[ch] = 1;
+//     // }
+//   }
+
+//   return charCount;
+// }
+
+// function deepEquals(objA, objB) {
+//   let result = true;
+
+//   for (let k in objA) {
+// // instructor suggested
+//     if (objA[k] !== objB[k]) {
+//       return false;
+//     }
+
+// //    result = result && (objA[k] === objB[k]);
+//   }
+
+//   return result;
+// }
+
+// function anagrams(stringA, stringB) {
+//   // cleanup word.replace(/[^\w]/g).toLowerCase()
+
+//   let objA = {};
+//   let objB = {};
+//   let cleanA = stringA.replace(/[^\w]/g).toLowerCase();
+//   let cleanB = stringB.replace(/[^\w]/g).toLowerCase();
+
+//   objA = getCharCount(cleanA);
+//   objB = getCharCount(cleanB);
+
+//   // instructor suggested
+//   if (Object.keys(objA).length !== Object.keys(objB).length) {
+//     return false;
+//   }
+
+//   return deepEquals(objA, objB);
+
+// }
+
+
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// instructor suggested
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  //console.log('result: ' + result)
+  return result;
+}
+
+
+// function capitalize(str) {
+//   let ar = str.split(' ');
+//   let arCap = ar.map(w => {
+//     // instructor suggested
+//     let str = w[0].toUpperCase() + w.slice(1)
+//     return str;
+
+//     // let str = ''
+//     // for (let i = 0; i < w.length; i++) {
+//     //   if (i === 0) {
+//     //     str += w[i].toUpperCase();
+//     //   } else {
+//     //     str += w[i];
+//     //   }
+//     // }
+//     // return str;
+//   });
+
+//   return arCap.join(' ');
+// }
 
 module.exports = capitalize;
