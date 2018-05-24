10d6ab61f420537afbc0aebf02c603899f7644ad
Carol Chung <cch5ng@gmail.com>
2018-05-23 22:30:54 -0700

    soln 0523 pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,93 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// working
+function pyramid(n) {
+  if (n === 1) {
+    console.log('#')
+  }
+
+  let colCount = (2 * n) - 1;
+  //console.log('colCount: ' + colCount)
+
+  if (n > 1) {
+    let spaceCount
+    for (let i = 0; i < n; i++) {
+      str = ''
+      spaceCount =  colCount - (1 + (i * 2))
+      //console.log('spaceCount: ' + spaceCount)
+      for (let j = 0; j < colCount; j++) {
+        if (spaceCount > 0) {
+          let mid = Math.floor(colCount / 2)
+          //console.log('mid: ' + mid)
+          //console.log('mid minus spaceCount/2: ' + (mid - (spaceCount / 2)).toString())
+          if (i === 0 && j === mid) {
+            str += '#';
+          } else if (i > 0 && j > (mid - (1 + (i * 2))/ 2) && j < (mid + (1 + (i * 2))/ 2)) {
+            str += '#';         
+          } else {
+            str += ' ';
+          }
+        }
+        if (spaceCount === 0) {
+          str += '#';
+        }
+      }
+      console.log(str)
+      spaceCount -= 2;
+
+    }
+  }
+
+}
+
+
+
+
+// function pyramid(n) {
+//   if (n === 1) {
+//     console.log('#')
+//   }
+
+//   let colCount = (2 * n) - 1;
+//   //console.log('colCount: ' + colCount)
+
+//   if (n > 1) {
+//     let spaceCount
+//     for (let i = 0; i < n; i++) {
+//       str = ''
+//       spaceCount =  colCount - (1 + (i * 2))
+//       //console.log('spaceCount: ' + spaceCount)
+//       for (let j = 0; j < colCount; j++) {
+//         if (spaceCount > 0) {
+//           let mid = Math.floor(colCount / 2)
+//           //console.log('mid: ' + mid)
+//           //console.log('mid minus spaceCount/2: ' + (mid - (spaceCount / 2)).toString())
+//           if (j === mid) {
+//             str += '#'
+//           } //else if (j > mid - ((1 + (i * 2)) / 2) || j < mid + ((1 + (i * 2)) / 2)) {
+//             //str += '#'
+//           //} 
+//           else {
+//             str += ' '
+//           }
+//           // if (j <= mid - spaceCount / 2) {
+//           //   str += ' '
+//           // } else if (j >= n - 1 - spaceCount / 2) {
+//           //   str += ' '
+//           // } else {
+//           //   str += '#'
+//           // }
+//         } else {
+//           str += '#'
+//         }
+//       }
+//       console.log(str)
+//       spaceCount -= 2
+
+//     }
+//   }
+
+// }
 
 module.exports = pyramid;
