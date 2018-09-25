79226fabd9b80e7266d132fd70034606918c57b2
cch5ng <cch5ng@gmail.com>
2018-09-24 17:51:20 -0700

    anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,60 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// https://www.w3schools.com/charsets/ref_html_ascii.asp
+// char codes A - Z; 65 - 90
+// char codes a - z; 97 - 122
+
+function anagrams(stringA, stringB) {
+  let obj1 = {};
+  let obj2 = {};
+
+  let string1 = stringA.toLowerCase();
+  let string2 = stringB.toLowerCase();
+
+  obj1 = strToObj(string1)
+  obj2 = strToObj(string2)
+
+  return deepEquals(obj1, obj2)
+}
+
+function strToObj(str) {
+  let obj = {};
+
+  for (let i= 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122 && str.char !== ' ') {
+      let char = str[i]
+      if (obj[char]) {
+        obj[char] += 1
+      } else {
+        obj[char] = 1
+      }
+    }
+  }
+
+  return obj
+}
+
+function deepEquals(obj1, obj2) {
+  let keys1 = []
+  let keys2 = []
+
+  keys1 = Object.keys(obj1)
+  keys2 = Object.keys(obj2)
+
+  if (keys1.length !== keys2.length) {
+    return false
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    let key = keys1[i]
+    if (obj1[key] !== obj2[key]) {
+      return false
+    }
+
+  }
+
+  return true
+}
 
 module.exports = anagrams;
