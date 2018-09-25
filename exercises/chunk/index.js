75d67a010630895ee84bd1a379258ffc6678c366
cch5ng <cch5ng@gmail.com>
2018-09-24 17:27:29 -0700

    array chunk prob

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  if (array.length <= size) {
+    return [array]
+  }
+
+  let result = []
+  for (let i = 0; i < array.length; i += size) {
+    let subAr = array.slice(i, i + size)
+    result.push(subAr)
+  }
+
+  //console.log('result', result)
+  return result;
+}
 
 module.exports = chunk;
