c02e7dcc38f16e25b5a2345491ebd363068ff672
Carol Chung <cch5ng@gmail.com>
2019-01-07 21:38:53 -0800

    array chunk soln

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,60 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+function chunk(array, size) {
+	let resAr = [];
+	let innerAr = [];
+
+	if (size > array.length) {
+		return [array];
+	}
+
+	for (let i = 0; i < array.length; i++) {
+		if (innerAr.length === size) {
+			resAr.push(innerAr);
+			innerAr = [];
+			innerAr.push(array[i]);
+		} else {
+			innerAr.push(array[i]);
+		}
+	}
+
+	if (innerAr.length) {
+		resAr.push(innerAr);
+	}
+
+	return resAr;
+}
+
+// function chunk(array, size) {
+// 	if (size > array.length) {
+// 		return [array];
+// 	}
+
+// 	let retAr = [];
+// 	let innerAr = [];
+// 	let counter = 1;
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (i === 0) {
+// 			innerAr.push(array[i]);
+// 		} else if (Math.floor(i / size) < counter) {
+// 			innerAr.push(array[i]);
+// 		} else if ((i / size) === counter) {
+// 			retAr.push(innerAr);
+// 			counter += 1;
+// 			innerAr = [];
+// 			innerAr.push(array[i]);
+// 		} else if (i === array.length) {
+// 			console.log('gets here')
+// 			retAr.push(innerAr);
+// 		}
+// 	}
+
+// 	console.log('retAr', retAr);
+// 	return retAr;
+
+// }
 
 module.exports = chunk;
